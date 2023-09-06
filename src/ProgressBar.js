68751efa5f7a49087f2b36ffaa8ee6df5b78d2f9
kris-panko <krispanko@gmail.com>
2023-09-06 11:56:06 -0400

    progress bar logic

--- src/ProgressBar.js
+++ src/ProgressBar.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const ProgressBar = ({ currentTime, duration }) => {
-  // Calculate progress and update the bar's width
+const ProgressBar = ({ currentTime, duration, handleSeek }) => {
+  const [sliderValue, setSliderValue] = useState(0);
+
+  const handleSliderChange = (e) => {
+    const newSliderValue = parseFloat(e.target.value);
+    setSliderValue(newSliderValue);
+    const newTime = (newSliderValue / 100) * duration;
+    handleSeek(newTime);
+  };
+
+  // Update the sliderValue when currentTime or duration changes
+  React.useEffect(() => {
+    const newSliderValue = (currentTime / duration) * 100;
+    setSliderValue(newSliderValue);
+  }, [currentTime, duration]);
 
   return (
     <div className="progress-bar">
-      <div className="progress" style={{ width: `calc((100% / ${duration}) * ${currentTime})` }}></div>
+      <div
+        className="progress"
+        style={{
+          width: `${sliderValue}%`,
+        }}
+      ></div>
+      <input
+        type="range"
+        min="0"
+        max="100"
+        step="0.01"
+        value={sliderValue}
+        className="progress-slider"
+        onChange={handleSliderChange}
+      />
     </div>
   );
 };
