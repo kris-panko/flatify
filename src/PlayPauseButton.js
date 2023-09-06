f9a573ef43c822d0be35e53d8f08fde7a2f4e2e9
Tano Paul <tanopaul38@gmail.com>
2023-09-06 16:04:44 -0400

    updated styling for Player.js

--- src/PlayPauseButton.js
+++ src/PlayPauseButton.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import playBtn from './assets/icons8-play-button-32.png'
 import pauseBtn from './assets/icons8-pause-button-32.png';
+import skipLeft from './assets/icons8-skip-to-start-32.png';
+import skipRight from './assets/icons8-end-32.png';
 
 const PlayPauseButton = ({ isPlaying, handlePlayPause }) => {
   const togglePlayPause = () => {
@@ -8,9 +10,13 @@ const PlayPauseButton = ({ isPlaying, handlePlayPause }) => {
   };
 
   return (
-    <button className="play-pause-button" onClick={togglePlayPause}>
-      {isPlaying ? <img src={pauseBtn} alt="Pause" /> : <img src={playBtn} alt="Play" />}
-    </button>
+    <div className='controls'>
+      <img src={skipLeft} alt='skip left' className='skip' />
+      <div className="play-pause-button" onClick={togglePlayPause}>
+        {isPlaying ? <img src={pauseBtn} alt="Pause" /> : <img src={playBtn} alt="Play" />}
+      </div>
+      <img src={skipRight} alt='skip right' className='skip' />
+    </div> 
   );
 };
 
