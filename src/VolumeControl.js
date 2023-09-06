806bdc2f7c540c79f1bd0c96254435fb063f6636
kris-panko <krispanko@gmail.com>
2023-09-06 14:44:36 -0400

    volume button show hide slider

--- src/VolumeControl.js
+++ src/VolumeControl.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import volumeIcon from './assets/icons8-volume-50.png';
 
-const VolumeControl = ({ volume, handleVolumeChange }) => {
-  // Handle volume control
+const VolumeControl = ({ volume, onVolumeChange }) => {
+  const [isSliderVisible, setSliderVisible] = React.useState(false);
+
+  const toggleSlider = () => {
+    setSliderVisible(!isSliderVisible);
+  };
 
   return (
     <div className="volume-control">
-      {/* Volume control UI */}
+      <div className="volume-button" onClick={toggleSlider}>
+        <img src={volumeIcon} alt="Volume" />
+      </div>
+      {isSliderVisible && (
+        <input
+          type="range"
+          min="0"
+          max="1"
+          step="0.01"
+          value={volume}
+          onChange={(e) => onVolumeChange(parseFloat(e.target.value))}
+          className="volume-slider"
+        />
+      )}
     </div>
   );
 };
